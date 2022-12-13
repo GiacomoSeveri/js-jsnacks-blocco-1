d1a7be4c6f4185bc826025edef24a81fa487b145
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-13 16:42:51 +0100

    added for

--- snack 1/js/script1.js
+++ snack 1/js/script1.js
@@ -1,75 +1,11 @@
 const amount = document.getElementById("sum");
 
-var a = prompt('inserisci un numero');
+let summation = 0;
 
- if (isNaN(a)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
+  for (let i = 0; i < 10; i++){
+     let n = parseInt(prompt("inserisci un numero"));
+     
+     summation += n;
+  }
 
- var b = prompt('inserisci un numero');
-
- if (isNaN(b)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var c = prompt('inserisci un numero');
-
- if (isNaN(c)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var d = prompt('inserisci un numero');
-
- if (isNaN(d)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
- 
- var e = prompt('inserisci un numero');
-
- if (isNaN(e)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var f = prompt('inserisci un numero');
-
- if (isNaN(f)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var g = prompt('inserisci un numero');
-
- if (isNaN(g)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var h = prompt('inserisci un numero');
-
- if (isNaN(h)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var i = prompt('inserisci un numero');
-
- if (isNaN(i)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
- var l = prompt('inserisci un numero');
-
- if (isNaN(l)) {
-    alert("QUESTO NON è UN NUMERO BIRICHINO");
-    // return;
- }
-
-let somma = (a + b + c + d + e + f + g + h + i + l);
-
-amount.innerHTML = `Il totale di tutti i numeri è ${somma}`;
+amount.innerHTML = `La somma totale è ${summation}`
